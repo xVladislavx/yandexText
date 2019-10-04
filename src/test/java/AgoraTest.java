2a3bef3cde4b1a219ffcd7b46909871798ea56ed
xVladislavx <ledovskihvladislav@gmail.com>
2019-10-04 17:46:07 +0300

    Вторая задача 0.5beta Changes: 1) TestNG вместо JUnit 2) Тест запускаются 10 раз 3) Поиск габаритов и порта отправления по атрибуту, а не id

--- src/test/java/AgoraTest.java
+++ src/test/java/AgoraTest.java
@@ -1,10 +1,15 @@
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
-import org.junit.Test;
+//import org.junit.Test;
+import com.codeborne.selenide.conditions.Text;
 import org.openqa.selenium.By;
+import org.testng.annotations.*;
 
+import javax.xml.bind.Element;
+import javax.xml.xpath.XPath;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -13,7 +18,7 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class AgoraTest {
 
-    @Test
+    @Test(invocationCount = 10)
     public void searchingToFrom() throws InterruptedException {
         Configuration.startMaximized = true;
         open("https://agorafreight.com/book/wizard");
@@ -31,12 +36,15 @@ public class AgoraTest {
         flag = $(By.className("header__buttons_language-switcher"));
         flag.click();
         Thread.sleep(1000);
+
         //Кликаем именно на английский язык
         $(By.className("Select-menu-outer")).find(By.id("react-select-2--option-0")).click();
+
         //Ждем немного
         Thread.sleep(3000);
         //Находим выпадающие меню From-To
         elements = $$(By.className("wizard__direction-block_country")).filterBy(text("Country"));
+
         //выбираем второй список
         from = elements.get(1);
         from.click();
@@ -49,39 +57,48 @@ public class AgoraTest {
                         System.out.println($(By.className("Select-option")));*/
         $$(By.className("Select-option")).findBy(id("react-select-16--option-2")).click();
         Thread.sleep(1000);
+
         //////////////////Опции доставки - откуда\\\\\\\\\\\\\\\\\\\\
         ////На инпут кликов не получается - вылезает ошибка
         //$$(By.className("form-check-input")).findBy(name("TO_RAILWAY_HUB")).click();
         //А на лейблы можно
         $$(By.className("form-check-label")).findBy(text("Sea port")).click();
         Thread.sleep(1000);
+
         //сначала надо открыть выпадающий список
         $(By.className("wizard__direction-block_info-hubs")).click();
         Thread.sleep(1000);
+
         //выбираем город порта исходный
-        //Выбираем нужный город
-        $$(By.className("Select-option")).findBy(id("react-select-14--option-17")).click();
+        System.out.println($$(By.className("Select-option")).filterBy(text("Shanghai, Shanghai, China")));
+        ElementsCollection element = $$(By.className("Select-option")).filterBy(text("Shanghai, Shanghai, China"));
+        element.get(0).click();
         Thread.sleep(1000);
+
+        //Открываем выпадающее меню для выбора нужного города
         //System.out.println($$(By.className("form-check-label")).findBy(text("Delivery address")));
         cities = $$(By.className("geosuggest__input"));
         cities.get(1).setValue("G");
         Thread.sleep(1000);
+
         $$(By.className("geosuggest__item")).findBy(text("Gabrovo, Болгария")).click();
         Thread.sleep(1000);
 
-        length = $$(By.className("form-control")).filterBy(id("f-element-9"));
+        //Выбираем поля ввода габаритов посылки в зависимости от атрибута
+        //System.out.println($$(By.className("form-control")).filterBy(attribute("placeholder")));
+        length = $$(By.className("form-control")).filterBy(attribute("placeholder", "L"));
         length.get(0).setValue("100");
 
-        width = $$(By.className("form-control")).filterBy(id("f-element-10"));
+        width = $$(By.className("form-control")).filterBy(attribute("placeholder", "W"));
         width.get(0).setValue("20");
 
-        height = $$(By.className("form-control")).filterBy(id("f-element-11"));
+        height = $$(By.className("form-control")).filterBy(attribute("placeholder", "H"));
         height.get(0).setValue("200");
 
-        weight = $$(By.className("form-control")).filterBy(id("f-element-12"));
+        weight = $$(By.className("form-control")).filterBy(attribute("placeholder", "KG"));
         weight.get(0).setValue("200");
 
-        Thread.sleep(10000);
+        Thread.sleep(3000);
 
         //////////////////Опции доставки - откуда\\\\\\\\\\\\\\\\\\\\
     }
