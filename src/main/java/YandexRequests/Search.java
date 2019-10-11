93d191f0cc3350371080fa2dd304530d9c8ded49
xVladislavx <ledovskihvladislav@gmail.com>
2019-10-11 18:39:27 +0300

    Третья задача правка промежуточная

--- src/main/java/YandexRequests/Search.java
+++ src/main/java/YandexRequests/Search.java
@@ -10,33 +10,30 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class Search {
-    private StringBuilder stringBuilder = new StringBuilder();
 
-    public StringBuilder findFirstInstances(List<String> requests) throws InterruptedException {
+    public StringBuilder findFirstInstances(String requests) throws InterruptedException {
+        String result;
         ElementsCollection requestsCollection;
         SelenideElement firstRequest;
         String value;
         int start;
         int finish;
 
-        for(int i = 0; i < requests.size(); i++){
-            $(By.className("input__control")).setValue(requests.get(i));
+        $(By.className("input__control")).setValue(requests);
 
-            Thread.sleep(1000);
+        Thread.sleep(1000);
 
-            requestsCollection = $$(By.className("suggest2-item"));
-            firstRequest = requestsCollection.get(0);
-            start = firstRequest.toString().indexOf("\"val\":\"") + 7;
-            finish = firstRequest.toString().indexOf("\"}}");
-            value = firstRequest.toString().substring(start, finish);
+        requestsCollection = $$(By.className("suggest2-item"));
+        //нужно весь список вытащить с виджетами погоды, а потом достать первый  из них
 
-            if(i != 2){
-                stringBuilder.append(value + ", ");
-            }
-            else {
-                stringBuilder.append(value);
-            }
-        }
-        return stringBuilder;
+
+/*
+        firstRequest = requestsCollection.get(0);
+        start = firstRequest.toString().indexOf("\"val\":\"") + 7;
+        finish = firstRequest.toString().indexOf("\"}}");
+        value = firstRequest.toString().substring(start, finish);
+*/
+
+        return null;
     }
 }
--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -8,22 +8,21 @@ public class YandexTest {
 
     @Test(invocationCount = 10)
     public void test() throws InterruptedException {
-        Requests requests = new Requests();
-        StringBuilder result;
+        StringBuilder result = new StringBuilder();
         Search search = new Search();
 
         Configuration.startMaximized = true;
         open("http://yandex.ru");
 
         //класс Requests отвечает за хранение искомых значений и формирования из них списка
-        requests.setRequest1("погода");
-        requests.setRequest2("Липецк");
-        requests.setRequest3("Лото");
+        search.findFirstInstances("погода");
 
-        requests.fillRequestsList();
+        search.findFirstInstances("Липецк");
+
+        search.findFirstInstances("Лото");
+
         //Класс Search принимает заполненный список из класса Requests и формирует в процессе поиска
         //формирует строку, которую выводим в консоль
-        result = search.findFirstInstances(requests.getRequestList());
 
         System.out.println(result);
     }
